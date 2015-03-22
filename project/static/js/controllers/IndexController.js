c6e27ede7bf7e33e046b77cabb421d85a170f733
= <stas_slesarev@mail.ru>
2015-03-22 18:28:23 +0200

    Add E2e tests

--- project/static/js/controllers/IndexController.js
+++ project/static/js/controllers/IndexController.js
@@ -10,12 +10,12 @@ indexController.controller('IndexController', ['$scope', 'Gen',
     $scope.checkMatches = function(index) {
       if ($scope.data.numbers[index].match) {
         // player finds match
-        $scope.data.numbers[index].match = 'success';
+        $scope.data.numbers[index].status = 'success';
         $scope.data.duplicatesNumber--;
 
       } else {
         // player loses
-        $scope.data.numbers[index].match = 'failure';
+        $scope.data.numbers[index].status = 'failure';
         $scope.data.duplicatesNumber = -1;
       }
 
@@ -26,11 +26,9 @@ indexController.controller('IndexController', ['$scope', 'Gen',
       switch ($scope.data.duplicatesNumber) {
         case 0:
           $scope.status.message = 'Victory!';
-          $scope.status.show = true;
           break;
         case -1:
           $scope.status.message = 'Failure = (';
-          $scope.status.show = true;
           break;
       }
       
@@ -43,8 +41,7 @@ indexController.controller('IndexController', ['$scope', 'Gen',
     function init() {
       $scope.data = Gen.generate();
       $scope.status = {
-        show: false,
-        message: ''
+        message: 'Game is going..'
       };
     }
 
---
+++ test/e2e/main-spec.js
@@ -0,0 +1,38 @@
+describe('Match Game', function () {
+  var hasClass = function (element, cls) {
+    return element.getAttribute('class').then(function (classes) {
+        return classes.split(' ').indexOf(cls) !== -1;
+    });
+  };
+
+  beforeEach(function () {
+    browser.get('http://127.0.0.1:5000/#/game');
+  });
+
+  it('should set .success class after click on Match number', function () {
+    var match = element.all(by.name('true')).first().click();
+    expect(hasClass(match, 'success')).toBeTruthy();
+  });
+
+  it('should set .failure class after click on Non-match number', function () {
+    var match = element(by.name('')).click();
+    expect(hasClass(match, 'failure')).toBeTruthy();
+  });
+
+  it('should set "Victory!" text to h1 after all matches were clicked', function () {
+    var matchesClicked = element.all(by.name('true')).each(function (match) {
+      match.click();
+    });
+
+    matchesClicked.then(function () {
+      expect( element(by.name('alert')).getText() ).toEqual('Victory!');      
+    });
+  });
+
+  it('should set "Failure = (" text to h1 after any Non-match was clicked', function () {
+    element.all(by.name('')).first().click();
+
+    expect( element(by.name('alert')).getText() ).toEqual('Failure = (');
+  });
+
+});
